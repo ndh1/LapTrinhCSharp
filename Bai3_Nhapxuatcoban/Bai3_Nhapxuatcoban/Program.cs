09558831124fb6f9c27c4b0646b98862efce7f51
huong <ndh1499@gmail.com>
2021-07-24 05:03:26 +0700

    test add

---
+++ Bai3_Nhapxuatcoban/Bai3_Nhapxuatcoban/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Bai3_Nhapxuatcoban
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Dinh Huong\n");
+            Console.WriteLine("Moi ban nhap ten ");
+            Console.WriteLine("Ten cua ban la " + Console.ReadLine());
+            Console.WriteLine("Moi ban nhap ngay sinh ");
+            Console.WriteLine("Ngay sinh cua ban la " + Console.ReadLine());
+            Console.ReadKey();
+        }
+    }
+}
